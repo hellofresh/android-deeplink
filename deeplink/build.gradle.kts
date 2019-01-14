39c64d0a596b4c9a6f47dc0a512a0e7c383f7a76
Henry Addo <henry@addhen.org>
2019-01-14 23:16:22 +0100

    Add config for uploading source and dokka doc as jar maven

--- deeplink/build.gradle.kts
+++ deeplink/build.gradle.kts
@@ -1,11 +1,13 @@
 import com.jfrog.bintray.gradle.BintrayExtension
 import org.gradle.api.publish.maven.MavenPom
+import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     id("com.android.library")
     id("kotlin-android")
     id("maven-publish")
     id("com.jfrog.bintray") version Versions.bintrayGradlePlugin
+    id("org.jetbrains.dokka-android") version Versions.dokkaAndroid
 }
 
 android {
@@ -45,14 +47,40 @@ fun MavenPom.addDependencies() = withXml {
 }
 
 val artifact = "$buildDir/outputs/aar/${Project.name}-${Project.version}-release.aar"
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(android.sourceSets.getByName("main").java.srcDirs)
+}
+
+val dokka by tasks.getting(DokkaTask::class) {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(dokka)
+}
+
+artifacts {
+    add("archives", sourcesJar)
+    add("archives", dokkaJar)
+}
+
+
 publishing {
 
     publications {
         register(Project.artifactId, MavenPublication::class) {
             groupId = Project.groupId
             artifactId = Project.artifactId
-            artifact(artifact)
             version = Project.version
+            artifact(artifact)
+            artifact(sourcesJar)
+            artifact(dokkaJar)
             pom.addDependencies()
         }
     }
@@ -63,19 +91,21 @@ bintray {
     key = System.getenv("BINTRAY_API_KEY") ?: ""
     publish = true
     setPublications(Project.artifactId)
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+    with(pkg) {
         repo = "maven"
         name = Project.name
-        userOrg = "hf-android-clan"
         websiteUrl = "https://github.com/hellofresh/android-deeplink/"
         githubRepo = "hellofresh/android-deeplink"
         vcsUrl = "https://github.com/hellofresh/android-deeplink/"
         description = "Deeplink parser library"
-        setLabels("kotlin", "Android", "Deep link")
-        setLicenses("Apache-2.0")
         desc = description
         publish = true
         githubRepo = "hellofresh/android-deeplink"
         githubReleaseNotesFile = "../CHANGELOG.md"
-    })
+        with(version) {
+            name = Project.version
+        }
+        setLabels("kotlin", "Android", "Deep link")
+        setLicenses("Apache-2.0")
+    }
 }
