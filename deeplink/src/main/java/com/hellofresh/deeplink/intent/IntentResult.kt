d092e4a99e39796aa9cfb58d661b9f1288e958cf
Kingsley Adio <adiksonline@gmail.com>
2019-04-11 16:11:54 +0200

    Rough stab v3 - Some questions already resolved - New questions up

--- deeplink/src/main/java/com/hellofresh/deeplink/intent/IntentResult.kt
+++ deeplink/src/main/java/com/hellofresh/deeplink/intent/IntentResult.kt
@@ -1,41 +1,37 @@
 package com.hellofresh.deeplink.intent
 
+import android.app.Activity
 import android.app.PendingIntent
 import android.app.TaskStackBuilder
 import android.content.Intent
 import com.hellofresh.deeplink.Environment
 
 class IntentResult private constructor(
-    private val environment: Environment,
+    private val env: Environment,
     private val taskStackBuilder: TaskStackBuilder,
     private val requiresAuth: Boolean,
     val isFallback: Boolean
 ) {
 
     fun start() {
-        // NEEDS ANSWERS! (Some things changed already. See below)
+        // NEEDS ANSWERS! (Some things already resolved and removed in this commit)
         // ====
         // Start intent or task stack builder, as the case may be! (everything now wrapped inside a task stack builder)
-        // Needs environment to know: (done)
-        // - If already authenticated: just start the direct intent
-        // - Else start the authenticator intent with direct forwarded
-        // If we have environment, then we probably don't need this context parameter? (done)
-        // Can/should we support intents other than activities? Services, broadcasts? (I think we shouldn't)
-        // Can we advance from auth activity to direct intent transparently? (I think not)
-        // What other alternatives do we have if we can't? (implemented a potential alternative)
+        // Unfortunately, task stack always restarts the whole stack
+        // regardless of whether or not stuff is already in the foreground, which makes the UX quite shitty :(
 
         // Do we need to expose a way to update the actual intent? Fill-in style
         // HF app adds BUNDLE_APPINDEXING_URI to intent post-parse
 
-        // How to expose API for authenticator intent?
-        // Do we need a singleton that already contains the env and auth intent?
+        // How to expose API for authenticator intent? (done)
         // ====
 
-        val context = environment.context
-        val alreadyAuthenticated = environment.isAuthenticated
+        val context = env.context
+        val alreadyAuthenticated = env.isAuthenticated
 
         if (requiresAuth && !alreadyAuthenticated) {
-            val authIntent = Intent() // Use actual intent. Should we expose an API for this? I think so
+            env as AuthEnvironment // Major key - Only cast when auth is actually needed :)
+            val authIntent = Intent(context, env.authenticator) // Works like this
                 .putExtra(
                     NEXT_INTENT,
                     taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
@@ -84,4 +80,9 @@ class IntentResult private constructor(
             return true
         }
     }
+}
+
+interface AuthEnvironment : Environment {
+
+    val authenticator: Class<out Activity> // Should we require a full intent instead?
 }
